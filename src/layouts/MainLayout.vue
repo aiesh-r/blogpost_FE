5218b7602831523e9897a69ddc3fcd0c005560cd
Ishani Rangana <ishani.r@softcodeit.com>
2022-02-09 10:17:50 +0530

    Initial Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Dev Blog
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Dev Blog</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
